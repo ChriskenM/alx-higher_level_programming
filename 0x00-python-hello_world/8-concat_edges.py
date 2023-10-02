9019ce1fdbb844fcef86a64a968041700e40cf8d
ChriskenM <chriskenmutuma0727@gmail.com>
2023-10-02 06:13:19 -0700

    debug

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bn/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
          language that combines remarkable power with very clear syntax"
-         str = str[39:67] + str[107:112] + str[:6]
-         print(str)
+         print(str[71:115] + str[0:6])
