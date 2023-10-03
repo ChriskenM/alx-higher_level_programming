bd5f05ff077e653e270984a7fc5d7f0be1ba6b7a
ChriskenM <chriskenmutuma0727@gmail.com>
2023-10-03 08:35:36 -0700

    debug

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
 
-def remove_char_at(str, n):
-        
-    if n < 0:
-        return (str)
-        return (str[:n] + str[n+1:])
+def remove_char_at(input_str, n):
+    if n < 0 or n >= len(input_str):
+        return input_str  
+    result_str = ""
+    for i in range(len(input_str)):
+        if i != n:
+            result_str += input_str[i]  # Append non-skipped characters
+                                                                                
+    return result_str  
+            
