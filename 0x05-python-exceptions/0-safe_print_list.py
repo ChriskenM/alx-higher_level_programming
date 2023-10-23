fcb0cb9edb1fc0b1d31713f0629d3ede5caced0a
ChriskenM <chriskenmutuma0727@gmail.com>
2023-10-23 04:30:38 -0700

    0-safe_print debug

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,13 +2,11 @@
 
 def safe_print_list(my_list=[], x=0):
     element_printed = 0
-
-    try:
-        for i in range(x):
-            print(my_list[], end="")
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end="")
             element_printed += 1
-    except IndexError:
-        pass
-
-    print()
-    return element_printed
+        except IndexError:
+            break
+    print("")
+    return (element_printed)
