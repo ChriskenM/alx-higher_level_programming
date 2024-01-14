e620110f7196cb5058486968aae9b81e41fdb746
ChriskenM <chriskenmutuma0727@gmail.com>
2024-01-14 04:00:54 -0800

    change file 5

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -16,8 +16,5 @@ if __name__ == '__main__':
     cur.execute("SELECT * FROM `cities` as `c` \
                 INNER JOIN `states` as `s` \
                 ON `c`.`state_id` = `s`.`id` \
-                ORDER BY `c`.`id`".format(sys.argv[4]))
-    states = cur.fetchall()
-
-    for state in states:
-        print(state)
+                ORDER BY `c`.`id`")
+    print(", ".join([ct[2] for ct in c.fetchall() if ct[4] == sys.argv[4]]))
