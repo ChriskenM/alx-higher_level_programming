304c7051892e9103742d5f7ea12b47fbce6ab6a8
ChriskenM <chriskenmutuma0727@gmail.com>
2024-01-29 03:29:53 -0800

    fixing 100-g

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -14,14 +14,14 @@ if __name__ == "__main__":
     repo_name = sys.argv[1]
     owner_name = sys.argv[2]
     url = "https://api.github.com/repos/{}/{}/commits".format(
-            owner_name, repo_name)
+        owner_name, repo_name
+    )
 
     response = requests.get(url)
     commits = response.json()
-    try:
-        for i in range(10):
-            print("{}: {}".format(
-                commits[i].get("sha"),
-                commits[i].get("commit").get("author").get("name")))
-    except IndexError:
-        pass
+
+    for i, commit in enumerate(commits[:10]):
+        print("{}: {}".format(
+            commit.get("sha"),
+            commit.get("commit").get("author").get("name")
+        ))
