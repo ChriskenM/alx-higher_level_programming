f9f3f1ad12f82e7bca2b45a38afc3eb080d37fb4
ChriskenM <chriskenmutuma0727@gmail.com>
2024-02-20 02:53:17 -0800

    all executables

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const apiUrl = process.argv[2];
+const characterId = 18;
+
+request.get(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    const filmData = JSON.parse(body).results;
+    const moviesWithWedge = filmData.filter((film) =>
+      film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)
+    );
+    console.log(`${moviesWithWedge.length}`);
+  }
+});
---
+++ 0x14-javascript-web_scraping/README.md
@@ -0,0 +1,14 @@
+[![js-semistandard-style](https://img.shields.io/badge/code%20style-semistandard-brightgreen.svg)](https://github.com/standard/semistandard)
+
+
+<h1> JavaScript - Web scraping </h1>
+
++ Scripting, API, JavaScript
++ Working with JSON data
++ The workflow of accessing the attributes of a simply-created JSON object.
++ request module
++ Modern JS
+
+
++ use #!/usr/bin/node
++ semistandard compliant. Rules of Standard + semicolons on top. Also as reference: AirBnB style
